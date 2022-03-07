241c63486588f81084b529f768816019211a31a0
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-08 00:10:07 +0100

    Create python.py

---
+++ python.py
@@ -0,0 +1,36 @@
+############################### STR
+
+# first_name = 'Mirek'
+# last_name = 'cipka'
+# full_name = first_name + last_name
+
+# # print("czesc" +name)
+
+# print(type(last_name))
+
+# print("hello " +full_name)
+
+################################ INT
+# wiek = 16
+
+# wiek = wiek + 1
+
+# print(type(wiek))
+
+# print("twoim wiekiem jest :" +str(wiek))
+
+##########FLOAT
+
+# wysokosc = 250.5
+
+# print("twoja wyskosc: " + str(wysokosc) +"cm")
+# print(type(wysokosc))
+
+
+############### BOOLEAN
+
+
+czlowiek = True
+print("jestes czlowiekiem? " +str(czlowiek))
+
+print(type(czlowiek))
