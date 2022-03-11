c3b86c9afd82e00c9423462d3c622efd5ba055a2
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-12 00:31:49 +0100

    if else elif

--- python.py
+++ python.py
@@ -1,17 +1,10 @@
-nazwa = "Miroslaw Klaus"
+wiek = int(input("Ile masz lat?: "))
 
-# pierwsze_imie = nazwa[7] liczba = litera w nazwie
-
-# imie = nazwa[:8] od poczatku do 8 [poczatek:3]
-# nazwisko = nazwa[9:] od 9 do konca [9:koniec]
-# dziwne_imie = nazwa[::1] pokazuje wszystko [poczatek:koniec:co ile]
-# odwrocone_imie = nazwa[::-1] pokazuje na odwrot [poczatek:koniec:-1]
-
-
-strona_internetowa1 = "http://google.com"
-strona_internetowa2 = "http://wikipedia.com"
-
-slice = slice(7,-4)
-
-print(strona_internetowa1[slice])
-print(strona_internetowa2[slice])
+if wiek == 100:
+    print("Ale jestes stary")
+elif wiek >= 18:
+    print("Jestes dorosly")
+elif wiek < 0:
+    print("Jescze sie nie urodziles")
+else:
+    print("Jestes bobaskiem")
