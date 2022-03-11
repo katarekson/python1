aae2730e0ca8c733f372aa6b09b6f42974fe956a
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-12 00:00:10 +0100

    math functions

--- python.py
+++ python.py
@@ -1,10 +1,16 @@
-name = input("Jak masz na imie: ")
-age = int(input("Ile masz lat: "))
-height = float(input("Jak wysoki jestes: "))
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
 
 
-print("Czesc"+name)
-
-print("Masz "+str(age)+" lat")
-
-print("Masz "+str(height)+" cm wysokosci")
+# print(round(pi)) 3 
+# print(math.ceil(pi)) 4
+# print(math.floor(pi)) 3
+# print(abs(pi))3.14
+# print(pow(pi, 2)) 3.14^2
+# print(math.sqrt(pi)) 3.14 do kwadratu
+# print(max(x, y, z)) najwieksze 
+# print(min(x, y, z)) najmniejsze 
