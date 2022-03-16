343d1fb9945a0e4d42973bd0b7a4433f488cdb75
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-17 00:53:17 +0100

    while loops

--- python.py
+++ python.py
@@ -1,10 +1,10 @@
 
-temperatura = int(input("Jaka jest temperatura na dworze: "))
+from unicodedata import name
 
-if not(temperatura >= 0 and temperatura <= 30):
-    print("Temperatura jest zla")
-    print("zostan w domu")
-elif not(temperatura < 0 or temperatura > 30):
-    print("Temperatura jest dobra!")
-    print("wyjdz z domu prosze")
+
+imie = None
+
+while not imie:
+    imie = input("Podaj swoje imie: ")
     
+print("hej " +imie)
