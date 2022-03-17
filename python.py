de26270d832a3d3b486576fe92a7d121161e9840
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-17 01:06:04 +0100

    nested loops

--- python.py
+++ python.py
@@ -1,15 +1,11 @@
-import time
+from symtable import Symbol
 
-# for i in range(10):
-#     print(i+1)
 
-# for i in range(50,100+1,2):
-#     print(i)
+rows = int(input("Jak duzo rzedow?: "))
+columns = int(input("Jak duzo Kolumn?: "))
+symbol = input("Podaj symbol jaki chcesz uzyc: ")
 
-# for i in "Miroslaw Klaus":
-#     print(i)
-
-for seconds in range(10,0,-1):
-    print(seconds)
-    time.sleep(2)
-print("Szczesliwego nowego roku!")
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end=" ")
+    print()
