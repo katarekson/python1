dc39c416ed3a7014404b9a5b4f3154aa1d38cdf7
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-20 01:13:09 +0100

    args

--- python.py
+++ python.py
@@ -1,9 +1,12 @@
-imie = "Mirek"
-
-
-def pokaz_imie():
-    # imie = "Klaus"
-    print(imie)
+def dodaj(*liczby):
+    sum = 0
+    liczby = list(liczby)
+    liczby[0] = 0
+    for i in liczby:
+        sum += i
+    return sum
     
-pokaz_imie()
-print(imie)
+    
+
+
+print(dodaj(1,2,3,10))
