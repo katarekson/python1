4681fbedbdf0dea1970336be99c1a386fcf59d35
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-21 00:50:04 +0100

    exepction handling

--- python.py
+++ python.py
@@ -1,17 +1,17 @@
-import random
-
-x = random.randint(1,100)
-y = random.random()
-
-mojalista = ['papier','kamien','nozycze']
-z = random.choice(mojalista)
-
-karty = [1,2,3,4,5,6,7,8,9,"J","Q","K","A"]
-random.shuffle(karty)
-
-print(karty)
-
-
-print(z)
-print(x)
-print(y) 
+try:
+    numerator = int(input("Podaj liczbe do podzielenia: "))
+    denominator = int(input("Podaj liczbe przez ktora bedziesz dzielil: "))
+    wynik = numerator / denominator
+except ZeroDivisionError as e:
+    print(e)
+    print("Nie mozesz dzielic przez zero!")
+except ValueError as e:
+    print(e)
+    print("Podawaj tylko liczby")
+except Exception as e:
+    print(e)
+    print("Cos poszlo nie tak")
+else:
+    print(wynik)
+finally:
+    print("To zawsze sie wykona!")
