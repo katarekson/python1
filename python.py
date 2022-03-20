4dc2c1a894f94dcd775d2dd771855163c50e0a61
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-21 00:33:20 +0100

    string format 3

--- python.py
+++ python.py
@@ -1,8 +1,11 @@
-imie = "Mirek"
+# liczba = 3.14159
 
-print("Nazywam sie {}".format(imie))
-print("Nazywam sie {:10}. Milo cie poznac!".format(imie))
-print("Nazywam sie {imie:10}. Milo cie poznac!".format(imie))
-print("Nazywam sie {:<10}. Milo cie poznac!".format(imie))
-print("Nazywam sie {:>10}. Milo cie poznac!".format(imie))
-print("Nazywam sie {:^10}. Milo cie poznac!".format(imie))
+# print("Liczba pi wynosi {:.2f}".format(liczba))
+
+liczba = 1000
+
+# print("Liczba wynosi {:,}".format(liczba))
+
+print("Liczba wynosi {:b}".format(liczba))
+print("Liczba wynosi {:X}".format(liczba))
+print("Liczba wynosi {:E}".format(liczba))
