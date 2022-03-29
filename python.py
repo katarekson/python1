c8ddf0107442e4a0df2c4784b8379f1789ca4cb9
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-29 21:37:48 +0200

    papier kamien nozyce

--- python.py
+++ python.py
@@ -1,1 +1,51 @@
-help("modules")
+import random
+
+
+while True:
+    wybory = ["papier","kamien","nozyce"]
+
+    komputer = random.choice(wybory)
+    gracz = None
+
+    while gracz not in wybory:
+        gracz = input("papier, kamien czy nozyce?: ").lower()
+
+
+    if gracz == komputer:
+        print("Komputer: ", komputer)
+        print("Gracz: ", gracz)
+        print("Remis")
+    elif gracz == "kamien":
+        if komputer == "papier":
+            print("Komputer: ", komputer)
+            print("Gracz: ", gracz)
+            print("przegrales")
+        if komputer == "nozyce":
+            print("Komputer: ", komputer)
+            print("Gracz: ", gracz)
+            print("Wygrales")
+    elif gracz == "papier":
+        if komputer == "nozyce":
+            print("Komputer: ", komputer)
+            print("Gracz: ", gracz)
+            print("przegrales")
+        if komputer == "kamien":
+            print("Komputer: ", komputer)
+            print("Gracz: ", gracz)
+            print("Wygrales")
+    elif gracz == "nozyce":
+        if komputer == "kamien":
+            print("Komputer: ", komputer)
+            print("Gracz: ", gracz)
+            print("przegrales")
+        if komputer == "papier":
+            print("Komputer: ", komputer)
+            print("Gracz: ", gracz)
+            print("Wygrales")
+            
+    zagraj_ponownie = input("Chcesz zagrac ponownie? (tak/nie) ").lower()
+    
+    if zagraj_ponownie != "tak":
+        break
+    
+print("Zegnaj")
