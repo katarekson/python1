a7aa699025eb8e619f57810b575708f39ea7a8a0
katarekson <85957231+katarekson@users.noreply.github.com>
2022-03-29 21:13:10 +0200

    modules

--- python.py
+++ python.py
@@ -1,17 +1,1 @@
-import os
-import shutil
-
-lokalizacja = "test.txt"
-
-try:
-    # os.remove(lokalizacja)
-    # os.rmdir(lokalizacja)
-    # shutil.rmtree(lokalizacja)
-except FileNotFoundError:
-    print("Plik nie zostal znaleziony")
-except PermissionError:
-    print("Nie masz permisji zeby to usunac")
-except OSError:
-    print("Nie mozesz tego usunac korzystajac z tej funkcji")
-else:
-    print(lokalizacja + " zostalo usuniete")
+help("modules")
