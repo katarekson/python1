4f636bde3a09f564dafd213afa139f51c914bc59
katarekson <85957231+katarekson@users.noreply.github.com>
2022-04-13 23:59:16 +0200

    super function

--- python.py
+++ python.py
@@ -1,24 +1,29 @@
-class Auto:
-    
-    def wlaczanie(self):
-        print("Silnik sie odpala")
-        return self
-    
-    def jezdzenie(self):
-        print("Jezdzisz autem")
-        return self
+class Prostokat:
+    def __init__(self,dlugosc,szerokosc):
+        self.dlugosc = dlugosc
+        self.szerokosc = szerokosc
         
-    def hamulec(self):
-        print("Naciskasz hamulec")
-        return self
-        
-    def wylacz(self):
-        print("Wylaczasz auto")
-        return self
-        
-auto = Auto()
 
-auto.wlaczanie()\
-    .jezdzenie()\
-    .hamulec()\
-    .wylacz()
+class Kwadrat(Prostokat):   
+    
+    def __init__(self,dlugosc,szerokosc):
+        super().__init__(dlugosc,szerokosc)
+    
+    def pole(self):
+        return self.dlugosc*self.szerokosc
+class Kostka(Prostokat):
+    def __init__(self,dlugosc,szerokosc,wysokosc):
+        super().__init__(dlugosc,szerokosc)
+        self.wysokosc = wysokosc
+    
+    
+    def objetosc(self):
+        return self.dlugosc*self.szerokosc*self.wysokosc
+
+
+kwadrat = Kwadrat(4, 2)
+kostka = Kostka(3, 1, 5)
+
+
+print(kwadrat.pole())
+print(kostka.objetosc())
