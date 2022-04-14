308a77bb462ac09344d4ba460a1bb1936eebe45b
katarekson <85957231+katarekson@users.noreply.github.com>
2022-04-14 23:42:41 +0200

    abstract class

--- python.py
+++ python.py
@@ -1,29 +1,35 @@
-class Prostokat:
-    def __init__(self,dlugosc,szerokosc):
-        self.dlugosc = dlugosc
-        self.szerokosc = szerokosc
+from abc import ABC, abstractmethod
+
+
+class Pojazd(ABC):
+    
+    @abstractmethod
+    def jedz(self):
+        pass
+    
+    @abstractmethod
+    def stop(self):
+        pass
+class Auto(Pojazd):
+    
+    def jedz(self):
+        print("Jedziesz autem")
         
-
-class Kwadrat(Prostokat):   
+    def stop(self):
+        print("Auto sie zatrzymalo")
+class Motor(Pojazd):
     
-    def __init__(self,dlugosc,szerokosc):
-        super().__init__(dlugosc,szerokosc)
-    
-    def pole(self):
-        return self.dlugosc*self.szerokosc
-class Kostka(Prostokat):
-    def __init__(self,dlugosc,szerokosc,wysokosc):
-        super().__init__(dlugosc,szerokosc)
-        self.wysokosc = wysokosc
-    
-    
-    def objetosc(self):
-        return self.dlugosc*self.szerokosc*self.wysokosc
+    def jedz(self):
+        print("Jedziesz motocyklem")
+        
+    def stop(self):
+        print("Ten motocykl sie zatrzymal")
 
 
-kwadrat = Kwadrat(4, 2)
-kostka = Kostka(3, 1, 5)
+# pojazd = Pojazd()
+auto = Auto()
+motor = Motor()
 
 
-print(kwadrat.pole())
-print(kostka.objetosc())
+auto.stop()
+motor.stop()
