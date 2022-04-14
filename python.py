bfdc4a441e50c5bc35074f33d4eed9b509da39a4
katarekson <85957231+katarekson@users.noreply.github.com>
2022-04-14 23:56:43 +0200

    duck typing

--- python.py
+++ python.py
@@ -1,28 +1,29 @@
-class Auto:
+class Kaczka:
     
-    kolor = None
+    def chodzenie(self):
+        print("Kaczka chodzi")
+        
+    def odglos(self):
+        print("Kaczka kwacze")
+        
+class Kurczak:
     
-class Motocykl:
+    def chodzenie(self):
+        print("Kurczak chodzi")
+        
+    def odglos(self):
+        print("Kurczak gdaka")
+        
     
-    kolor = None
+class Osoba():
     
-    
-def zmien_kolor(pojazd, kolor):
-    
-    pojazd.kolor = kolor
-    
-auto_1 = Auto()
-auto_2 = Auto()
-auto_3 = Auto()
+    def zlapanie(self, kaczka):
+        kaczka.chodzenie()
+        kaczka.odglos()
+        print("Zlapales ja!!")
+        
+kaczka = Kaczka()
+kurczak = Kurczak()
+osoba = Osoba()
 
-rower_1 = Motocykl()
-
-zmien_kolor(auto_1, "czerwony")
-zmien_kolor(auto_2, "fioletowy")
-zmien_kolor(auto_3, "czarny")
-zmien_kolor(rower_1, "czarny")
-
-print(auto_1.kolor)
-print(auto_2.kolor)
-print(auto_3.kolor)
-print(rower_1.kolor)
+osoba.zlapanie(kurczak)
