8afa736766c91e0b9dbe3a69eeef5ca2e2b4c8db
katarekson <85957231+katarekson@users.noreply.github.com>
2022-04-14 23:47:59 +0200

    objects as arguments

--- python.py
+++ python.py
@@ -1,35 +1,28 @@
-from abc import ABC, abstractmethod
-
-
-class Pojazd(ABC):
+class Auto:
     
-    @abstractmethod
-    def jedz(self):
-        pass
+    kolor = None
     
-    @abstractmethod
-    def stop(self):
-        pass
-class Auto(Pojazd):
+class Motocykl:
     
-    def jedz(self):
-        print("Jedziesz autem")
-        
-    def stop(self):
-        print("Auto sie zatrzymalo")
-class Motor(Pojazd):
+    kolor = None
     
-    def jedz(self):
-        print("Jedziesz motocyklem")
-        
-    def stop(self):
-        print("Ten motocykl sie zatrzymal")
+    
+def zmien_kolor(pojazd, kolor):
+    
+    pojazd.kolor = kolor
+    
+auto_1 = Auto()
+auto_2 = Auto()
+auto_3 = Auto()
 
+rower_1 = Motocykl()
 
-# pojazd = Pojazd()
-auto = Auto()
-motor = Motor()
+zmien_kolor(auto_1, "czerwony")
+zmien_kolor(auto_2, "fioletowy")
+zmien_kolor(auto_3, "czarny")
+zmien_kolor(rower_1, "czarny")
 
-
-auto.stop()
-motor.stop()
+print(auto_1.kolor)
+print(auto_2.kolor)
+print(auto_3.kolor)
+print(rower_1.kolor)
