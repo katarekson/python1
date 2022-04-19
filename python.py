adba39afa9e91edd76aec56fa3c22ca0bc665fc5
katarekson <85957231+katarekson@users.noreply.github.com>
2022-04-19 22:57:29 +0200

    sort

--- python.py
+++ python.py
@@ -1,11 +1,43 @@
-# def podwoj(x):
-#     return x * 2
+# # sort() method = uzywane przy listach(lists)
+# # sort() funcitons = uzywane przy iteracjach(iterables)
 
-# print(podwoj(10))
+# # studenci = ["Mirek","Michal","Kacper","Krystian"]
+# studenci = ("Mirek","Michal","Kacper","Krystian")
 
-podwoj = lambda x: x * 2
-pomnoz = lambda x, y: x * y
-dodaj = lambda x, y, z: x + y + z
-pelne_imie = lambda imie, nazwisko: imie+" "+nazwisko
-sprawdzenie_wieku = lambda wiek: print("Mozesz sie zarejestrowac") if wiek >= 18 else print("Nie masz ukonczonych 18 lat")
-print(sprawdzenie_wieku(19))
+
+# # studenci.sort(reverse=True)
+# sorted_studenci = sorted(studenci, reverse=True)
+
+# for i in sorted_studenci:
+#     print(i)
+
+#-------------------------------------------------------------#
+#-------------------------------------------------------------#
+# # studenci = [("Mirek", "5", 16),
+# #             ("Kacper","5", 17),
+# #             ("Michal","4",16),
+# #             ("Krystian","6",16)]
+
+# ocena = lambda oceny:oceny[1]
+# imie = lambda imiona:imiona[0]
+
+# studenci.sort(key=imie, reverse=True)
+
+
+# for i in studenci:
+#     print(i)
+
+#==============================================================#
+
+    
+    
+studenci = (("Mirek", "5", 16),
+             ("Kacper","5", 17),
+             ("Michal","4",16),
+             ("Krystian","6",16))
+
+wiek = lambda wiek:wiek[2]
+sorted_studenci = sorted(studenci, key=wiek)
+
+for i in sorted_studenci:
+    print(i)
