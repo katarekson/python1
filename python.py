3f57091e252ea67eea07f659c13f80b66a5a404d
katarekson <85957231+katarekson@users.noreply.github.com>
2022-04-19 22:43:24 +0200

    lambda function

--- python.py
+++ python.py
@@ -1,21 +1,11 @@
-# #Akceptuje funkcje jako argument czyli funkcje glosno lub cicho jako argument po prostu
+# def podwoj(x):
+#     return x * 2
 
-# def glosno(text):
-#     return text.upper()
+# print(podwoj(10))
 
-# def cicho(text):
-#     return text.lower()
-
-# def czesc(func):
-#     text = func("Witam")
-#     print(text)
-    
-# czesc(cicho)
-
-def dzielnik(x):
-    def dzielna(y):
-        return y / x
-    return dzielna
-
-podziel = dzielnik(3)
-print(podziel(11))
+podwoj = lambda x: x * 2
+pomnoz = lambda x, y: x * y
+dodaj = lambda x, y, z: x + y + z
+pelne_imie = lambda imie, nazwisko: imie+" "+nazwisko
+sprawdzenie_wieku = lambda wiek: print("Mozesz sie zarejestrowac") if wiek >= 18 else print("Nie masz ukonczonych 18 lat")
+print(sprawdzenie_wieku(19))
