1cb8a22fca0367a4801dc5004f617103318a1a91
katarekson <85957231+katarekson@users.noreply.github.com>
2022-04-19 23:05:00 +0200

    map

--- python.py
+++ python.py
@@ -1,43 +1,12 @@
-# # sort() method = uzywane przy listach(lists)
-# # sort() funcitons = uzywane przy iteracjach(iterables)
+sklep = [("Podkoszulka", 50.00),
+         ("Spodnie", 129.00),
+         ("Bluza", 259.00),
+         ("Skarpetki", 23.00)]
 
-# # studenci = ["Mirek","Michal","Kacper","Krystian"]
-# studenci = ("Mirek","Michal","Kacper","Krystian")
+to_pln = lambda data: (data[0],data[1]*4.64)
+to_euro = lambda data: (data[0],data[1]/4.64)
 
+sklep_euro = list(map(to_euro, sklep))
 
-# # studenci.sort(reverse=True)
-# sorted_studenci = sorted(studenci, reverse=True)
-
-# for i in sorted_studenci:
-#     print(i)
-
-#-------------------------------------------------------------#
-#-------------------------------------------------------------#
-# # studenci = [("Mirek", "5", 16),
-# #             ("Kacper","5", 17),
-# #             ("Michal","4",16),
-# #             ("Krystian","6",16)]
-
-# ocena = lambda oceny:oceny[1]
-# imie = lambda imiona:imiona[0]
-
-# studenci.sort(key=imie, reverse=True)
-
-
-# for i in studenci:
-#     print(i)
-
-#==============================================================#
-
-    
-    
-studenci = (("Mirek", "5", 16),
-             ("Kacper","5", 17),
-             ("Michal","4",16),
-             ("Krystian","6",16))
-
-wiek = lambda wiek:wiek[2]
-sorted_studenci = sorted(studenci, key=wiek)
-
-for i in sorted_studenci:
+for i in sklep_euro:
     print(i)
