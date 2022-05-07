084031483deb454e6519219e4e26132cc777a556
katarekson <85957231+katarekson@users.noreply.github.com>
2022-05-08 01:08:07 +0200

    if name == main

--- python.py
+++ python.py
@@ -1,19 +1,9 @@
-# nazwy_uzytkownika = ["Mirek", "Kacper", "Michal"]
-# haslo = ("zaq1@WSX", "niezlehaslo2137", "kulhaslo2305")
+#----------------------------------#
+# if __name__ == "__main__":
+#----------------------------------#
 
-# uzytkownicy = dict(zip(nazwy_uzytkownika, haslo))
+def main():
+    print("Czesc!")
 
-# print(type(uzytkownicy))
-
-# for key,value in uzytkownicy.items():
-#     print(key+" : "+value)
-    
-    
-nazwy_uzytkownika = ["Mirek", "Kacper", "Michal"]
-haslo = ("zaq1@WSX", "niezlehaslo2137", "kulhaslo2305")
-data_logowania = ["01/03/2020","06/02/2021","16/03/2021"]
-
-uzytkownicy = zip(nazwy_uzytkownika,haslo,data_logowania)
-
-for i in uzytkownicy:
-    print(i)
+if __name__ == "__main__":
+    main()
