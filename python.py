be82d7107350261199e6c31d0bd20d3f8d9ebe08
katarekson <85957231+katarekson@users.noreply.github.com>
2022-05-22 22:46:10 +0200

    listbox

--- python.py
+++ python.py
@@ -1,27 +1,62 @@
 from tkinter import *
 
 def submit():
-    print("Temperatura wynosi: "+ str(scale.get())+ " celcjusza")
-
+    #print("Zamowiles: "+(listbox.get(listbox.curselection())))
+    jedzenie = []
+    
+    for index in listbox.curselection():
+        jedzenie.insert(index,listbox.get(index))
+        
+    for index in jedzenie:
+        print("Zamowiles: "+index)
+        
+    
+def add():
+    listbox.insert(listbox.size(),entryBox.get())
+    listbox.config(height=listbox.size())
+    
+def delete():
+    #listbox.delete(listbox.curselection())
+    
+    for index in reversed(listbox.curselection()):
+        listbox.delete(index)
+        
+    listbox.config(height=listbox.size())
+    
+    
 okno = Tk()
 
-scale = Scale(okno,
-              from_=-10,
-              to=50,
-              length=500,
-              orient=HORIZONTAL,
-              font = ('Comic Sans', 15),
-              tickinterval=10, #Liczby na skali
-              showvalue = 0, #ukrywa wartosc
-              troughcolor = 'green', #kolor slidera
-              fg= 'red',
-              bg= 'black',
-              )
+listbox = Listbox(okno, 
+                  bg ="Black",
+                  fg = "White",
+                  font =('Constantia', 20),
+                  width = 20,
+                  selectmode=MULTIPLE, #pozwala na wybranie kilku
+                  )
 
-scale.set(((scale['from']-scale['to'])/2)+scale['to'])
 
-scale.pack()
 
-button = Button(okno,text='Wyslij', command=submit)
-button.pack()
+
+listbox.pack()
+
+listbox.insert(1,"pizza")
+listbox.insert(2,"burgir")
+listbox.insert(1,"losos")
+listbox.insert(1,"pomidorowa")
+listbox.insert(1,"szpagetii")
+
+listbox.config(height=listbox.size())
+
+entryBox = Entry(okno)
+entryBox.pack()
+
+submit_button = Button(okno,text="submit",command=submit)
+submit_button.pack()
+
+add_button = Button(okno,text="dodaj",command=add)
+add_button.pack()
+
+del_button = Button(okno,text="usun",command=delete)
+del_button.pack()
+
 okno.mainloop()
